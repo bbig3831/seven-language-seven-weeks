4af01826c0d69c74e489fadf4a2b8d8046eaf4b5
Ben <ben.bigelow@tutanota.com>
2022-07-09 15:16:45 -0500

    Add file for day 1 of Ruby

---
+++ day1.rb
@@ -0,0 +1,20 @@
+# Code for day 1 exercises
+# Print the string "Hello, world."
+puts 'Hello, world.'
+
+# For the string “Hello, Ruby,” find the index of the word “Ruby.”
+'Hello, Ruby'.index('Ruby')
+
+# Print your name ten times.
+i = 0
+while i < 10
+  puts 'Ben'
+  i += 1
+end
+
+# Print the string “This is sentence number 1,” where the number 1 changes from 1 to 10.
+i = 1
+while i < 11
+  puts "This is sentence number #{i}."
+  i += 1
+end
