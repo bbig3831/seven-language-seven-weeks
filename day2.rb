10e51748a74f437e6e945a93b367d134251e4f50
Ben <ben.bigelow@tutanota.com>
2022-07-10 23:00:59 -0500

    Add code for day 2 of Ruby

---
+++ day2.rb
@@ -0,0 +1,5 @@
+# Code for Ruby day 2
+# Accessing files with and without code blocks
+# Translate hash to array: hash.to_a
+# Transalte array to hash: array.to_h
+# Can you iterate through a hash? Yes, .each, .each_pair, etc.
